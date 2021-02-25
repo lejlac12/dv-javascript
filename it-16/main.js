b398e7b61ab94821ae684a80cdffd4d2d6735441
lejlac12 <76840973+lejlac12@users.noreply.github.com>
2021-02-25 22:13:54 +0100

    Video 16

---
+++ it-16/main.js
@@ -0,0 +1,10 @@
+var ime = "Lejla";
+var prezime = "CuRt";
+
+if (ime == "Lejla") {
+    if (prezime == "Curt") {
+        document.write("Mozete uci na sajt !");
+    } else {
+        document.write("Ne mozete uci na sajt !");
+    }
+}
