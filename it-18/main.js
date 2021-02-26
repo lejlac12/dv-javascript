df9a5fbc63618daa5f740d1a063fa0d4ae45d9f6
lejlac12 <76840973+lejlac12@users.noreply.github.com>
2021-02-26 07:37:01 +0100

    Video 18

---
+++ it-18/main.js
@@ -0,0 +1,15 @@
+var ime = "...";
+
+switch (ime) {
+    case "Lejla":
+        document.write("Hej Lejla!");
+        break;
+    case "Edin":
+        document.write("Hej Edin!");
+        break;
+    case "Selma":
+        document.write("Hej Selma");
+        break;
+    default:
+        document.write("Hej!");
+}
