24d70cee7205beb5655f14415d99e0d2b3d4ddc7
lejlac12 <76840973+lejlac12@users.noreply.github.com>
2021-02-27 08:24:32 +0100

    Video 25

---
+++ it-25/main.js
@@ -0,0 +1,10 @@
+function osoba(ime, godine) {
+    this.ime = ime;
+    this.ime = ime;
+}
+
+var Lejla = new osoba("Lejla Curt,", 24);
+var Edin = new osoba(" Edin Jasarevic", 30);
+
+document.write(Lejla.ime);
+document.write(Edin.ime);
