87d8113ddfcc7360ebf0caa6d54c5e0955d44702
lejlac12 <76840973+lejlac12@users.noreply.github.com>
2021-02-28 17:49:10 +0100

    Video 32

---
+++ it-32/main.js
@@ -0,0 +1,8 @@
+var voce = new Array("jabuka", "kruska", "breskva", "banana");
+
+voce.reverse();
+voce.push("jagoda", "malina");
+voce.sort();
+
+var string1 = voce.join();
+document.write(string1);
